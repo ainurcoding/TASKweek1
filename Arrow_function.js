404f6c8ebd416922ecddde3da5ffc6ec64307f30
Ainur <ainurridwan98@gmail.com>
2021-04-29 09:51:15 +0700

    add function task

---
+++ Arrow_function.js
@@ -0,0 +1,5 @@
+let sayHello = (name, age) => {
+    return "Halo nama saya " + name +" usia saya "+ age;
+}
+
+console.log(sayHello("ainur",23)); 
---
+++ Regular_function.js
@@ -0,0 +1,5 @@
+function SayHello(name, age){
+    console.log("Halo nama saya : "+ name + " usia saya : " + age);
+}
+
+SayHello("Ainur","23");
