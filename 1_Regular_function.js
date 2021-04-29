d7eb9003d355fc9310dd70057d5aeafc39377af7
Ainur <ainurridwan98@gmail.com>
2021-04-29 19:50:24 +0700

    add Function Task

---
+++ 1_Regular_function.js
@@ -0,0 +1,14 @@
+// function introduceSelf(){
+//     let name = document.getElementById("name").value;
+//     let from  = document.getElementById("from").value;
+//     document.getElementById("output").innerHTML = theName = !name ? "tell us about you" : "hay "+name+" are u from "+ from;
+// }
+
+introduceSelf = () =>{
+    let name = document.getElementById("name").value;
+    let from  = document.getElementById("from").value;
+    document.getElementById("output").innerHTML = theName = !name ? "tell us about you" : "hay "+name+" are u from "+ from;
+}
+
+
+
